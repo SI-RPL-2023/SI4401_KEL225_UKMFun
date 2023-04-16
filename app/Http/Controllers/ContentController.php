1e1ed9fc6b2b9cb2c10605082ed8b258eed31bb9
ramyaaninditaa <ramya.anindita@gmail.com>
2023-04-16 22:05:08 +0700

    [S2-PBI14] Halaman Utama Telutizen resolve #35

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -10,6 +10,39 @@ use Illuminate\Support\Facades\Auth;
 
 class ContentController extends Controller
 {
+    public function index_telutizen()
+    {
+        $contents = Content::all();
+        $title = 'Home';
+
+
+        // dd($contents->all());
+        // $pendaftaran = Pendaftaran::find(Auth::user()->id_user);
+        $pendaftaran = Pendaftaran::where('id_mahasiswa', Auth::user()->id_user)->get();
+
+        // if ($pendaftaran == null) {
+        //     $pendaftaran = [];
+        // }
+
+
+        return view('telutizen.home', compact('contents', 'pendaftaran', 'title'));
+    }
+
+    public function detail_ukm($id_ukm)
+    {
+        $contents = Content::where('id_ukm',  $id_ukm)->latest()->get();
+        $title = $contents[0]->nama_ukm;
+        $contents = $contents[0];
+        // dd($contents->all()[0]->nama_ukm);
+        // $pendaftaran = Pendaftaran::find(Auth::user()->id_user);
+        $pendaftaran = Pendaftaran::where('id_mahasiswa', Auth::user()->id_user)->get();
+        // dd($pendaftaran);
+        if ($pendaftaran == null) {
+            $pendaftaran = [];
+        }
+        
+        return view('telutizen.detail-ukm', compact('contents', 'pendaftaran', 'title'));
+    }
     
     public function store(Request $request)
     {
