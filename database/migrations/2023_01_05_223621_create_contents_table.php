18ec17c540d324adfc615386afc0a0cddc6cc5fe
maysbkti11 <maysbkti@gmail.com>
2023-05-08 11:28:44 +0700

    [S2-PBI17] Daftar UKM resolve #37

--- database/migrations/2023_01_05_223621_create_contents_table.php
+++ database/migrations/2023_01_05_223621_create_contents_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('contents', function (Blueprint $table) {
             $table->id('id_content')->unsigned()->autoIncrement();
             $table->integer('id_ukm');
-            $table->string('nama_ukm', 255);
+            $table->longText('nama_ukm');
             $table->string('deskripsi', 5000);
             $table->string('logo', 255);
             $table->string('jumbotron', 255);
